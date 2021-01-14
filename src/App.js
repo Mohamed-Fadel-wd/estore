f049541239498f4f06c6cfa19e917939d80eb167
Mohamed-Fadel-wd <66232581+Mohamed-Fadel-wd@users.noreply.github.com>
2021-01-14 13:38:29 +0200

    first swing at react routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import {Route, Switch} from "react-router-dom"
 
 import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
+
+const HatsPage = () => (
+ <div>
+   <h1>hatspage</h1>
+ </div>
+)
+
+const App = () => (
+  <div className='App'>
+
+      <Switch>
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/hats" component={HatsPage}/>
+      </Switch>
+      
+        
       </div>
-    );
-  }
-}
+)
 
 export default App;
