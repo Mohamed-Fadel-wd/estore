aa76cd74eca249fe28c1facc2ff4a6a44b85ac19
Mohamed-Fadel-wd <66232581+Mohamed-Fadel-wd@users.noreply.github.com>
2021-01-15 11:43:20 +0200

    basic header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import {Route, Switch} from "react-router-dom"
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from "./pages/shop/shop.component"
-
-import './App.css';
-
-
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import "./App.css";
 
 const App = () => (
-  <div className='App'>
-
-      <Switch>
-      <Route exact path="/" component={HomePage}/>
-      <Route path="/shop" component={ShopPage}/>
-      </Switch>
-      
-        
-      </div>
-)
+  <div className="App">
+    <Header />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
+    </Switch>
+  </div>
+);
 
 export default App;
